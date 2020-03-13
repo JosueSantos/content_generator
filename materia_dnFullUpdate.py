f9a18a0d59b8b292247b6751a2df3bd2ca68415c
Josue Dos Santos Marques <josue.santos@verdesmares.com.br>
2020-03-13 10:06:30 -0300

    criado view_dn

--- materia_dnFullUpdate.py
+++ materia_dnFullUpdate.py
@@ -9,15 +9,19 @@ db = connection.dn_backup
 materia_dn = db.materia_dn
 
 
-for materia in materia_dn.find():
+for materia in materia_dn.find(no_cursor_timeout=True):
 	if isinstance(materia['id_dn'], str):
 		dateCreated = datetime.datetime.strptime(materia['dateCreated'], "%Y-%m-%d %H:%M:%S")
+
+		id_dn = materia['id_dn']
+		if ('/' in materia['id_dn']):
+			id_dn = materia['id_dn'].split('1.')[-1]
 
 		data = {
 			'dateCreated': dateCreated,
 			'link_rel': int(materia['link_rel']),
 			'link_rel_interno': int(materia['link_rel_interno']),
-			'id_dn': int(materia['id_dn'])
+			'id_dn': int(id_dn)
 		}
 
 		print("Materia DN " + str(materia['id_dn']))
@@ -31,3 +35,5 @@ for materia in materia_dn.find():
 		)
 
 	print("DN " + str(materia['id_dn']))
+
+materia_dn.close()
---
+++ view_dn.py
@@ -0,0 +1,32 @@
+import pymongo
+import datetime
+
+import pandas as pd
+
+connection = pymongo.MongoClient(
+	host="localhost",
+	port=27017
+)
+db = connection.dn_backup
+materia_dn = db.materia_dn
+
+print('--------------------------- Total de materias - ' + str(materia_dn.count_documents({})))
+
+ano_materia = materia_dn.aggregate([
+	{
+		"$match" : { "dateCreated": {"$lt": datetime.datetime.now()} }
+	},
+	{
+		"$group" : {
+			"_id" : { "ano_materia": { "$year" : "$dateCreated" } },
+			"count": { "$sum" : 1 }
+		}
+	},
+	{
+		"$sort" : { "_id.ano_materia" : -1 }
+	}
+])
+
+for item in ano_materia:
+	if item['_id']['ano_materia'] > 1100:
+		print( str(item['_id']['ano_materia']) + ' - ' + str(item['count']))
